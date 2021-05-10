05ab27ccccb6889f9fb626870bbddc8a070f4d5a
Isaac Tadokoro <risinginiris2017@gmail.com>
2021-05-10 22:13:18 +0800

    新增MARYTCalendar老黄历插件

---
+++ MarytBOT/bot_plugins/MARYTOldCalendar.py
@@ -0,0 +1,22 @@
+from nonebot.command import CommandSession
+from nonebot.experimental.plugin import on_command
+import time
+from random import randint
+
+__plugin_name__ = 'MARYTOldCalendar'
+__plugin_usage__ = '''
+用法：
+输入“老黄历”随机抽取一对宜忌事项
+'''
+goodList = ["向阿苟表白","东风牛逼！","和弧弧贴贴","和吼姆一起提桶跑路","摸Create 101","造自动化流水线","种小麦","种马铃薯","种胡萝卜","养动物","打末影龙","吃饱喝足","摸了"]
+badList = ["上原神贴吧PVP","和微博女拳对线","上知乎键政","看嘭哈布打手冲","在pixiv找色图","看真夏夜的银梦","看潮汕英豪传","看cookie⭐","当乐子人"]
+async def todayGoodAndBad():
+    timeNow = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    goodThing = goodList[randint(0,len(goodList)-1)]
+    badThing = badList[randint(0,len(badList)-1)]
+    return "现在是："+timeNow+"，\n"+"今日老黄历：\n"+"宜："+goodThing+"\n"+"忌："+badThing
+
+@on_command('黄历',aliases=('老黄历'),permission=lambda sender: (not sender.is_privatechat) or sender.is_superuser)
+async def _(session: CommandSession):
+    todayGAB = await todayGoodAndBad()
+    await session.send(todayGAB)
--- MarytBOT/bot_plugins/MARYTfaq.py
+++ MarytBOT/bot_plugins/MARYTfaq.py
@@ -14,17 +14,18 @@ async def _(session: CommandSession):
 1. 鼠标指向物品，按键盘U查用途，R查配方。
 2. 查阅mcmod百科可以得到大部分问题的答案。
 3. 对于Create101整合包，你还可以阅读游戏加载、暂停界面的“雪尼的完美教室”提示。
-4. 实在没有再到群里提问。
-''')
+4. 实在没有再到群里提问。''')
 
 @on_command('客户端',aliases=('客户端下载'),permission=lambda sender: (not sender.is_privatechat) or sender.is_superuser)
 async def _(session: CommandSession):
     await session.send('''目前可以从QQ群文件中下载到文件名【客户端】开头的客户端压缩包。
 转发给自己的QQ号，再下载，可以让下载变快。
-推荐使用MARTY唯二指定压缩软件合作伙伴7-zip和Bandizip解压客户端！
-''')
+推荐使用MARTY唯二指定压缩软件合作伙伴7-zip和Bandizip解压客户端！''')
 
-@on_command('登录',aliases=('登陆','登入'),permission=lambda sender: (not sender.is_privatechat) or sender.is_superuser)
+@on_command('注册',aliases=('登陆','登入','登录'),permission=lambda sender: (not sender.is_privatechat) or sender.is_superuser)
 async def _(session: CommandSession):
-    await session.send('''目前服务器使用的是离线登录，只要在启动器填入你的游戏ID就可以啦！
-''')
+    await session.send('''按以下步骤操作：
+1. 进入官网maryt.world注册Maryt通行证，并在官网登录通行证，这很重要！；
+2. 进入皮肤站skin.maryt.world，绑定一个用户名，就是你之后在服务器中的ID；
+3. 进入启动器，点击启动器左上角“没有游戏账户”，点击右下角+号，在弹出窗口中的“用户名”输入Maryt通行证邮箱，“密码”输入Maryt通行证密码，点击确定，等待登陆完成。
+4. 点击启动器左上角按钮，然后点击右下角“启动游戏”即可。进入游戏后无需再登陆，直接玩就行了。''')
